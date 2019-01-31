3bb792d0467c1bad6b61920fbce3dd5e2eb2d7df
coki <xiao230@126.com>
2019-01-31 16:32:41 +0800

    init

---
+++ eureka-server/src/main/java/eurekaServer/EurekaServerMain.java
@@ -0,0 +1,12 @@
+package eurekaServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerMain {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerMain.class);
+    }
+}
